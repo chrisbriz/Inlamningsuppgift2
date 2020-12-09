404c2a92855d269759c9cf4cc4f1dcff169ccd35
chrisbriz <christopher.brizet@outlook.com>
2020-12-09 08:34:42 +0100

    Add project files.

---
+++ Inlamningsuppgift/Participant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inlamningsuppgift
+{
+    class Participant
+    {
+        public Participant(string status, string birthMonth, int age, string hobby, string typeOfHabitation, string favoriteCandy, string education, string favoriteSeason, string favoriteFood, string favoriteAnimal)
+        {
+            Status = status;
+            BirthMonth = birthMonth;
+            Age = age;
+            Hobby = hobby;
+            TypeOfHabitation = typeOfHabitation;
+            FavoriteCandy = favoriteCandy;
+            FavoriteAnimal = favoriteAnimal;
+            FavoriteSeason = favoriteSeason;
+            Education = education;
+            FavoriteFood = favoriteFood;
+        }
+
+        public string Status { get; set; }
+        public string BirthMonth { get; set; }
+        public string TypeOfHabitation { get; set; }
+        public int Age { get; set; }
+        public string Hobby { get; set; }
+        public string FavoriteCandy { get; set; }
+        public string Education { get; set; }
+        public string FavoriteSeason { get; set; }
+        public string FavoriteFood { get; set; }
+        public string FavoriteAnimal { get; set; }
+
+    }
+}
---
+++ Inlamningsuppgift/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inlamningsuppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        public static void Start()
+        {
+            //create login function with denial message if password is wrong
+            Console.WriteLine("Enter Password: ");
+            string password = Console.ReadLine();
+            password = password.ToLower();
+
+            if (password != "csharks")
+            {
+                Console.WriteLine("Wrong password");
+                Start();
+            }
+            else
+            {
+                MainMenu();
+            }
+
+
+            
+
+            //create List of participants
+            List<Participant> lists = new List<Participant>();
+        }
+
+        //create menu (list all participants, get 10 generic details about participants, get specific string about each participants
+        public static void MainMenu()
+        {
+
+        }
+    }
+}
